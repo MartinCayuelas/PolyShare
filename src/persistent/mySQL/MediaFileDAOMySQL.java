487ab8ea3d311e445beb6700a124bd61bb1fd437
Martin Cayuelas <martincayuelas@gmail.com>
2019-01-11 08:57:15 +0100

    Add files via upload

---
+++ src/persistent/mySQL/MediaFileDAOMySQL.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class MediaFileDAOMySQL extends MediaFileDAO {
+
+    /**
+     * Default constructor
+     */
+    public MediaFileDAOMySQL() {
+    }
+
+}

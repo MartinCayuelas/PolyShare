d24b44f3d7b2f9ec813f9e0c3ee81a9ef7ecd511
Luca <l.debeir@me.com>
2019-01-11 10:03:44 +0100

    rajout BdConnection

--- src/database/BdConnection.java
+++ src/database/BdConnection.java
@@ -1,11 +1,31 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.*;
 
 /**
  * <b>BdConnection</b> is used to connect into the database
  */
 public class BdConnection {
+	
+	//Objet Connection
+	private static Connection connect;
+		 
+	//Méthode qui va nous retourner notre instance et la créer si elle n'existe pas
+	public static Connection getInstance(){
+	    if(connect == null){
+	    	try {
+		    	Properties props = new Properties();
+				props.setProperty("ssl","true");
+		        connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8889/polysharebase","root","root");
+		    } catch (SQLException e) {
+		    	e.printStackTrace();
+		    }
+		 }      
+		 return connect;
+	}
 
     /**
      * Default constructor

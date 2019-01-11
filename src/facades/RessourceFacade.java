0c2cf0c95f8e0ed4b12392f04713a4b6a4dafcc7
Martin Cayuelas <martincayuelas@gmail.com>
2019-01-11 08:54:34 +0100

    Add files via upload

---
+++ src/facades/RessourceFacade.java
@@ -0,0 +1,124 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class RessourceFacade {
+
+    /**
+     * Default constructor
+     */
+    public RessourceFacade() {
+    }
+
+
+    /**
+     * @param text 
+     * @return
+     */
+    public void addAnswer(String text) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param text 
+     * @return
+     */
+    public void addQuestion(String text) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param text 
+     * @return
+     */
+    public void updateAnswer(String text) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void updateQuestion() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void closePost() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void likeQ() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void likeA() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void unlikeQ() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void unlikeA() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param text 
+     * @return
+     */
+    public void saveMediaFile(String text) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void deleteMediaFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void likeMediaFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void unlikeMediaFile() {
+        // TODO implement here
+        return null;
+    }
+
+}

582a6237d34eae55ac2f28cf107c83461a63f89d
Martin Cayuelas <martincayuelas@gmail.com>
2019-01-11 08:56:56 +0100

    Add files via upload

---
+++ src/persistent/DAO/MediaFileDAO.java
@@ -0,0 +1,70 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class MediaFileDAO {
+
+    /**
+     * Default constructor
+     */
+    public MediaFileDAO() {
+    }
+
+
+    /**
+     * @param d 
+     * @return
+     */
+    public void createMediaFile(MediaFile d) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param s 
+     * @return
+     */
+    public MediaFile findByName(String s) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param d 
+     * @return
+     */
+    public void delete(MediaFile d) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param d 
+     * @return
+     */
+    public void update(MediaFile d) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param m 
+     * @return
+     */
+    public void like(MediaFile m) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param m 
+     * @return
+     */
+    public void unlike(MediaFile m) {
+        // TODO implement here
+        return null;
+    }
+
+}

f3b84d34141d462930c97caa86841c767b3069aa
Martin Cayuelas <martincayuelas@gmail.com>
2019-01-11 08:58:48 +0100

    Add files via upload

---
+++ src/ui/mediaFile/MediaFileController.java
@@ -0,0 +1,56 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class MediaFileController {
+
+    /**
+     * Default constructor
+     */
+    public MediaFileController() {
+    }
+
+
+    /**
+     * @return
+     */
+    public void upload() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void delete() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void save() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void like() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void unlike() {
+        // TODO implement here
+        return null;
+    }
+
+}
---
+++ src/ui/mediaFile/MediaFileFXML.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class MediaFileFXML {
+
+    /**
+     * Default constructor
+     */
+    public MediaFileFXML() {
+    }
+
+    /**
+     * 
+     */
+    public MediaFileController controller;
+
+
+}

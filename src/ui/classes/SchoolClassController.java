935a0a95c2b9c59289f90e414b6f99294a5e0535
Nathaniut <nathan.guillaud@orange.fr>
2019-01-12 11:21:38 +0100

    UI : Ajout vue topic + suppression MainSchoolClass

--- src/ui/classes/SchoolClassController.java
+++ src/ui/classes/SchoolClassController.java
@@ -1,12 +1,59 @@
 package ui.classes;
 
+import java.io.IOException;
 import java.util.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * 
  */
-public class SchoolClassController {
+public class SchoolClassController extends Application {
+	
+	//Partie du lancement de la fenêtre
+	private Stage primaryStage;
+    private AnchorPane classLayout;
 
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("ClassApp");
+
+        initClassLayout();
+	}
+	
+	public void initClassLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader(SchoolClassController.class.getResource("SchoolClass.fxml"));
+            classLayout = (AnchorPane) loader.load();
+            
+            // Show the scene containing the class layout.
+            Scene scene = new Scene(classLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	/**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	//Partie du controller
     /**
      * Default constructor
      */
--- src/ui/topic/TopicController.java
+++ src/ui/topic/TopicController.java
@@ -1,12 +1,60 @@
 package ui.topic;
 
+import java.io.IOException;
 import java.util.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import ui.classes.SchoolClassController;
 
 /**
  * 
  */
-public class TopicController {
+public class TopicController extends Application {
+	
+	//Partie du lancement de la fenêtre
+	private Stage primaryStage;
+    private AnchorPane topicLayout;
 
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("ClassApp");
+
+        initClassLayout();
+	}
+	
+	public void initClassLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader(TopicController.class.getResource("Topic.fxml"));
+            topicLayout = (AnchorPane) loader.load();
+            
+            // Show the scene containing the class layout.
+            Scene scene = new Scene(topicLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	/**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	//Partie du controller
     /**
      * Default constructor
      */

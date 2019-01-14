16585ecea29c602f83575fe19090950670dd1125
Martin Cayuelas <martincayuelas@gmail.com>
2019-01-14 23:42:46 +0100

    Netoyage des prints dans fichiers

--- src/ui/skill/AddSkill/addNewSkillController.java
+++ src/ui/skill/AddSkill/addNewSkillController.java
@@ -19,20 +19,20 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import ui.Router;
 import ui.skill.SkillCell;
 
 /**
  * @author martinCayuelas
  */
 
-
 public class addNewSkillController implements Initializable {
-	
+
 	private LoginFacade loginFacade = new LoginFacade();
 	private SchoolClassFacade schoolClassFacade = new SchoolClassFacade();
 	ObservableList<String> SchoolClassObservableList;
-	
-	
+	Router r = Router.getInstance();
+
 	@FXML
 	private TextField nameSkill;
 	// @FXML private TextField nameSkill;
@@ -41,46 +41,47 @@ public class addNewSkillController implements Initializable {
 
 	@FXML
 	private Spinner<Integer> mySpinner;
-	@FXML 
+	@FXML
 	private ChoiceBox<String> choiceBoxClass;
 
-	
-public void addSkill() throws DisconnectedStudentException {
-		//int idStudent = loginFacade.getConnectedStudent().getId();
-		//Mettre idStudent à la place de 1
-	
-		System.out.println(nameSkill.getText());
-		System.out.println(choiceBoxClass.getValue());
+	public void addSkill() throws DisconnectedStudentException {
+		// int idStudent = loginFacade.getConnectedStudent().getId();
+		// Mettre idStudent à la place de 1
+
+		// System.out.println(nameSkill.getText());
+		// System.out.println(choiceBoxClass.getValue());
 		SchoolClass lClass = schoolClassFacade.findSchoolClass(choiceBoxClass.getValue());
-		System.out.println(lClass.getIdSchoolClass() + " - " +lClass.getNameSchoolClass());
+		// System.out.println(lClass.getIdSchoolClass() + " - "
+		// +lClass.getNameSchoolClass());
 		int idClass = lClass.getIdSchoolClass();
 		Skill s = new Skill(0, nameSkill.getText(), mySpinner.getValue(), 1, idClass);
 		skillFacade.addSkill(s);
-		System.out.println("Ajout Reussi");
-	}
-private void initSpinner() {
-	
-	
-    mySpinner.setValueFactory(
-        new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5)
-    );
-}
+		
+		
+		//Quand on appuie sur Add ça doit renvoyer vers la page des skills
 
-@Override
-public void initialize(URL arg0, ResourceBundle arg1) {
-	// TODO Auto-generated method stub
-	
-	initSpinner();
-	List<SchoolClass> sc = new ArrayList<>();
-	sc = schoolClassFacade.getAllSchoolClass();
-	SchoolClassObservableList = FXCollections.observableArrayList();
-	for(SchoolClass c : sc) {
-		System.out.println("Class : " +c.getNameSchoolClass());
-		SchoolClassObservableList.add(c.getNameSchoolClass());
 	}
-	
-	this.choiceBoxClass.setItems(SchoolClassObservableList);
-	
-}
+
+	private void initSpinner() {
+
+		mySpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5));
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+		initSpinner();
+		List<SchoolClass> sc = new ArrayList<>();
+		sc = schoolClassFacade.getAllSchoolClass();
+		SchoolClassObservableList = FXCollections.observableArrayList();
+		for (SchoolClass c : sc) {
+			//System.out.println("Class : " + c.getNameSchoolClass());
+			SchoolClassObservableList.add(c.getNameSchoolClass());
+		}
+
+		this.choiceBoxClass.setItems(SchoolClassObservableList);
+
+	}
 
 }

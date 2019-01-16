ed46feae09cbd6a3817ae72ef8fbf802d319204b
julien-roumagnac <julienroumagnac1@gmail.com>
2019-01-16 17:26:16 +0100

    ajout class MyDate

---
+++ src/application/classesApp/MyDate.java
@@ -0,0 +1,59 @@
+package application.classesApp;
+
+public class MyDate {
+	
+	private String jour;
+	private String mois;
+	private String année;
+	
+	// this class is date class to help us work with sql the sql date format is "AAAA-MM-JJ" so we keep year month and day .
+	public MyDate(String jour, String mois, String année) {
+		super();
+		this.jour = jour;
+		this.mois = mois;
+		this.année = année;
+	}
+	public MyDate ( String date) {
+		this.année= date.substring(0, 4);
+		this.mois= date.substring(6, 8);
+		this.jour= date.substring(10,12);
+	}
+	
+	// this function return the date of this object date in the sql format needed to create a date in SQL
+	public String getSQLDate() {
+		String res;
+		res = this.getAnnée().concat("--").concat(this.getMois()).concat("--").concat(this.getJour());
+		
+		return res ;
+	}
+	// return the date with a format used for the scene like 2019/06/15
+	public String getShowingDate() {
+		String res;
+		res = this.getAnnée().concat("/").concat(this.getMois()).concat("/").concat(this.getJour());
+		
+		return res ;
+	}
+	
+	public String getJour() {
+		return jour;
+	}
+	
+	public void setJour(String jour) {
+		this.jour = jour;
+	}
+	public String getMois() {
+		return mois;
+	}
+	public void setMois(String mois) {
+		this.mois = mois;
+	}
+	public String getAnnée() {
+		return année;
+	}
+	public void setAnnée(String année) {
+		this.année = année;
+	}
+	
+	
+
+}
